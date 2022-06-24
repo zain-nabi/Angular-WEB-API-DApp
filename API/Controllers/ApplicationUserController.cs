db44507431f593263eabaa946aeb2f5815903aeb
Zain Nabi <zainolnabi88@gmail.com>
2022-06-24 08:52:31 +0200

    Initial Commit

---
+++ API/Controllers/ApplicationUserController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ApplicationUserController(DataContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+            return await _context.ApplicationUsers.ToListAsync();
+        }
+
+        //api/Users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetUser(int id)
+        {
+            return await _context.ApplicationUsers.FindAsync(id);
+        }
+    }
+}
